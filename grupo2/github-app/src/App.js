cc4a31bf0ea68a201702ae370815f55790b5f2c3
Bruno Duarte <brunoarduarte@gmail.com>
2021-07-25 16:16:41 -0300

    Fix deploy problem 2

--- grupo2/github-app/src/App.js
+++ grupo2/github-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="user" element={<UserPage />} />
+      <Route path="/github-app/user" element={<UserPage />} />
     </Routes>
   );
 }
