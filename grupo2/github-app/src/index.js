e0b52855f4aca689d03a161fd61e080519f9db79
Bruno Duarte <brunoarduarte@gmail.com>
2021-07-25 15:51:22 -0300

    Fix deploy problem

--- grupo2/github-app/src/index.js
+++ grupo2/github-app/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import store from './store';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/github-app">
     <Provider store={ store }>
       <App />
     </Provider>
